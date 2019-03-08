9335e67eeccb4e46032e5029f7196d350b793ffa
ills5771 <ilyayelizarov1@gmail.com>
2019-03-08 09:49:18 -0800

    added addSmurf action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -15,6 +15,8 @@ import axios from "axios";
 */
 export const GETTING_SMURFS = "GETTING_SMURFS";
 export const GET_SMURFS = "GET_SMURF";
+export const ADDING_SMURF = "ADDING_SMURF";
+export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => dispatch => {
   dispatch({ type: GETTING_SMURFS });
@@ -22,6 +24,18 @@ export const getSmurfs = () => dispatch => {
     .get("http://localhost:3333/smurfs")
     .then(res => {
       dispatch({ type: GET_SMURFS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADDING_SMURF });
+  axios
+    .post("http://localhost:3333/smurfs", smurf)
+    .then(res => {
+      dispatch({ type: ADD_SMURF, payload: res.data });
     })
     .catch(err => {
       console.log(err);
