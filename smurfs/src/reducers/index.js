1000de04b8b8b3710fe87fa98f6ab468a3b8b493
ills5771 <ilyayelizarov1@gmail.com>
2019-03-08 08:40:06 -0800

    added reducer with cases for GET_SMURFS and GETTING_SMURFS

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,9 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import React from "react";
 
+import { GET_SMURFS, GETTING_SMURFS } from "../actions";
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -12,8 +14,33 @@
    deletingSmurf: false
    error: null
  }
+ 
 */
-
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  getSmurfs: false,
+  error: null
+};
+const reducer = (initialState, action) => {
+  switch (action.type) {
+    case GETTING_SMURFS:
+      return {
+        ...state,
+        gettingSmurfs: true
+      };
+    case GET_SMURFS:
+      return {
+        ...state,
+        getSmurfs: true,
+        error: null,
+        smurfs: action.payload
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
