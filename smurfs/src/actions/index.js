1c438a58dcaa56072b6d2a3779d71b5d7a52be20
ills5771 <ilyayelizarov1@gmail.com>
2019-03-08 08:33:12 -0800

    added getSmurfs action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from "axios";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +13,17 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const GETTING_SMURFS = "GETTING_SMURFS";
+export const GET_SMURFS = "ADD_SMURF";
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GETTING_SMURFS });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: GET_SMURFS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
